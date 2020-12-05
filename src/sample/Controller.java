74ae40e06bed8971351290ddd621d2d3426fd63c
MathiasMadsen <m1999@live.dk>
2020-12-06 00:24:23 +0100

    update GUI

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,59 +2,11 @@ package sample;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
-import java.awt.*;
+import org.w3c.dom.ls.LSOutput;
 
 public class Controller {
-
     @FXML
-    TextArea Tekst;
+    private TextArea textAreaTekst;
 
-
-    public void Knap0(){
-        Tekst.appendText("0");
-    }
-    public void Knap1(){
-        Tekst.appendText("1");
-    }
-    public void Knap2(){
-        Tekst.appendText("2");
-    }
-    public void Knap3(){
-        Tekst.appendText("3");
-    }
-    public void Knap4(){
-        Tekst.appendText("4");
-    }
-    public void Knap5(){
-        Tekst.appendText("5");
-    }
-    public void Knap6(){
-        Tekst.appendText("6");
-    }
-    public void Knap7(){
-        Tekst.appendText("7");
-    }
-    public void Knap8(){
-        Tekst.appendText("8");
-    }
-    public void Knap9(){
-        Tekst.appendText("9");
-    }
-    public void knapPLus(){
-        Tekst.appendText(" + ");
-    }
-    public void knapminus(){
-        Tekst.appendText(" - ");
-    }
-    public void knapDividere(){
-        Tekst.appendText(" / ");
-    }
-    public void knapGange(){
-        Tekst.appendText(" * ");
-    }
-
-    public void clear(){
-        Tekst.clear();
-    }
 
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Lommeregner 2.0");
-        primaryStage.setScene(new Scene(root, 208, 298));
+        primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.setResizable(false);
         primaryStage.show();
     }
