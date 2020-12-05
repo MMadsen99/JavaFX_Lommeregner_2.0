b056bcf7b3b087d25e0cfadb8059b432c76d06dd
MathiasMadsen <m1999@live.dk>
2020-12-06 00:44:16 +0100

    metoder er tilføjet

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -8,5 +8,47 @@ public class Controller {
     @FXML
     private TextArea textAreaTekst;
 
+    public void Knap0(){
+        textAreaTekst.appendText("0");
+    }
+    public void Knap1(){
+        textAreaTekst.appendText("1");
+    }
+    public void Knap2(){
+        textAreaTekst.appendText("2");
+    }
+    public void Knap3(){
+        textAreaTekst.appendText("3");
+    }
+    public void Knap4(){
+        textAreaTekst.appendText("4");
+    }
+    public void Knap5(){
+        textAreaTekst.appendText("5");
+    }
+    public void Knap6(){
+        textAreaTekst.appendText("6");
+    }
+    public void Knap7(){
+        textAreaTekst.appendText("7");
+    }
+    public void Knap8(){
+        textAreaTekst.appendText("8");
+    }
+    public void Knap9(){
+        textAreaTekst.appendText("9");
+    }
+    public void knapPLus(){
+        textAreaTekst.appendText(" + ");
+    }
+    public void knapMinus(){
+        textAreaTekst.appendText(" - ");
+    }
+    public void knapDividere(){
+        textAreaTekst.appendText(" / ");
+    }
+    public void knapGange(){
+        textAreaTekst.appendText(" * ");
+    }
 
 }
